045fa4bd3f4e1d58505f725081bffec9aab8bb1e
Tomasz Braczyński <tomekbraczynski@o2.pl>
2017-11-06 23:51:52 +0100

    - zdefiniowano kilka styli - utworzono strony (w większości puste) - dodano hamburger menu - zaimplementowano mechanizm przekierowujący na odpowiednią podstronę - dla widoku mobilnego dodano belkę powiadomień - zmieniono konfigurację i wymagania aplikacji

--- MobilnyOpiekun/MainPage.xaml.cs
+++ MobilnyOpiekun/MainPage.xaml.cs
@@ -24,7 +24,55 @@ namespace MobilnyOpiekun
     {
         public MainPage()
         {
-            this.InitializeComponent();
+            InitializeComponent();
+            ShowStatusBar();
+        }
+
+        private void btnHamburger_Click(object sender, RoutedEventArgs e)
+        {
+            HamburgerMenu.IsPaneOpen = !HamburgerMenu.IsPaneOpen;
+        }
+
+        private void mnuItem_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            switch ((sender as StackPanel).Name)
+            {
+                case "mnuStronaGlowna":
+                    txtTytulStrony.Text = "Strona główna";
+                    zawartosc.Navigate(typeof(Views.StronaGlowna));
+                    break;
+                case "mnuStatystyka":
+                    txtTytulStrony.Text = "Statystyka ruchu";
+                    zawartosc.Navigate(typeof(Views.Statystyka));
+                    break;
+                case "mnuWezwijPomoc":
+                    txtTytulStrony.Text = "Wzywanie pomocy";
+                    zawartosc.Navigate(typeof(Views.WezwijPomoc));
+                    break;
+                case "mnuUstawienia":
+                    txtTytulStrony.Text = "Ustawienia";
+                    zawartosc.Navigate(typeof(Views.Ustawienia));
+                    break;
+                case "mnuPomoc":
+                    txtTytulStrony.Text = "Pomoc";
+                    zawartosc.Navigate(typeof(Views.Pomoc));
+                    break;
+            }
+            if (HamburgerMenu.IsPaneOpen)
+            {
+                HamburgerMenu.IsPaneOpen = false;
+            }
+        }
+
+        // show the StatusBar
+        private async void ShowStatusBar()
+        {
+            if (Windows.Foundation.Metadata.ApiInformation.IsTypePresent("Windows.UI.ViewManagement.StatusBar"))
+            {
+                var statusbar = Windows.UI.ViewManagement.StatusBar.GetForCurrentView();
+                await statusbar.ShowAsync();
+                statusbar.BackgroundOpacity = 1;
+            }
         }
     }
 }
---
+++ MobilnyOpiekun/Views/Ustawienia.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.Contacts;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace MobilnyOpiekun.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Ustawienia : Page
+    {
+        ApplicationDataContainer localSettings;
+        ApplicationDataCompositeValue composite;
+
+        public Ustawienia()
+        {
+            InitializeComponent();
+
+            localSettings = ApplicationData.Current.LocalSettings;
+
+            // Composite setting
+            composite = (ApplicationDataCompositeValue)localSettings.Values["exampleCompositeSetting"];
+
+            if (composite == null)
+            {
+                // No data - create empty compositeValue
+                composite = new ApplicationDataCompositeValue();
+            }
+            else
+            {
+                // Access data in composite["intVal"] and composite["strVal"]
+            }
+        }
+
+        private void btnOdrzucUstawienia_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(MainPage));
+        }
+
+        private void btnZachowajUstawienia_Click(object sender, RoutedEventArgs e)
+        {
+            localSettings.Values["exampleCompositeSetting"] = composite;
+            Frame.Navigate(typeof(MainPage));
+        }
+
+        private async void btnWybierzKontakt_ClickAsync(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                ContactPicker contactPicker = new ContactPicker();
+                contactPicker.CommitButtonText = "Select";
+                contactPicker.SelectionMode = ContactSelectionMode.Fields;
+                contactPicker.DesiredFieldsWithContactFieldType.Add(ContactFieldType.PhoneNumber);
+
+                Contact contact = await contactPicker.PickContactAsync();
+                if (contact != null)
+                {
+                    string numer = contact.Phones.FirstOrDefault().Number;
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
